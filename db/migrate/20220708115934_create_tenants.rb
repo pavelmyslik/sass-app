239d4b49670ef6dc976799e3667ffd1505585950
pavelmyslik <pavel.myslik@email.cz>
2022-07-08 14:06:25 +0200

    Devise added

---
+++ db/migrate/20220708115934_create_tenants.rb
@@ -0,0 +1,10 @@
+class CreateTenants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tenants do |t|
+      t.references :tenant, index: true, foreign_key: true
+      t.string :name
+      t.timestamps
+    end
+    add_index :tenants, :name
+  end
+end
---
+++ db/migrate/20220708120122_create_tenants_users_join_table.rb
@@ -0,0 +1,7 @@
+class CreateTenantsUsersJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :tenants, :users do |t|
+      t.index [:tenant_id, :user_id]
+    end
+  end
+end
